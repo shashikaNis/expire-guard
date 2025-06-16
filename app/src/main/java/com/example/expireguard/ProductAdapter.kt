b888688538ac374303dcf46bd6e18137fdda582f
buddhikapathiraja <buddhika@fcodelabs.com>
2025-06-16 19:52:16 +0530

    implemented delete product

--- app/src/main/java/com/example/expireguard/ProductAdapter.kt
+++ app/src/main/java/com/example/expireguard/ProductAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.expireguard
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -51,7 +56,28 @@ class ProductAdapter(private val productList: ArrayList<Product>) :
                             true
                         }
                         R.id.action_delete -> {
-                            // Handle delete
+                        var db = FirebaseFirestore.getInstance()
+                            var userId = FirebaseAuth.getInstance().currentUser?.uid
+                            AlertDialog.Builder(view.context)
+                                .setTitle("Delete Product")
+                                .setMessage("Are you sure you want to delete ${product.name}?")
+                                .setPositiveButton("Yes") { _, _ ->
+                                    // Call a function to delete the product from Firestore
+                                    product.id.let { productId ->
+                                        db.collection("users").document(userId!!).collection("products")
+                                            .document(productId)
+                                            .delete()
+                                            .addOnSuccessListener {
+                                                Toast.makeText(view.context, "Product deleted", Toast.LENGTH_SHORT).show()
+                                            }
+                                            .addOnFailureListener { e ->
+                                                Log.w("Delete Product", "Error deleting product", e)
+                                                Toast.makeText(view.context, "Failed to delete product", Toast.LENGTH_SHORT).show()
+                                            }
+                                    }
+                                }
+                                .setNegativeButton("No", null)
+                                .show()
                             true
                         }
                         else -> false
